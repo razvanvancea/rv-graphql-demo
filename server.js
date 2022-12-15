8e4a867f1dcb711f2d5c4da1061bed30ed16be38
Razvan Vancea <razvan.vancea@zitec.com>
2022-12-16 00:47:06 +0200

    update data

--- server.js
+++ server.js
@@ -11,33 +11,33 @@ const {
 const app = express()
 
 const authors = [
-	{ id: 1, name: 'A. L. Smith', email: 'alsmith@gmail.com' },
-	{ id: 2, name: 'J. R. R. Tolbach',email: 'tolbach@gmail.com' },
-	{ id: 3, name: 'John Wick', email: 'jwick@gmail.com' }
+	{ id: 1, name: 'C. Tangana', email: 'c.tangana@gmail.com', homeTown: 'Madrid, Spain'},
+	{ id: 2, name: 'Arctic Monkeys',email: 'arctic@gmail.com', homeTown: 'Sheffield, UK' },
+	{ id: 3, name: 'Metallica', email: 'metallica@gmail.com', homeTown: 'Lost Angeles, USA' }
 ]
 
-const books = [
-	{ id: 1, name: 'Harry Potter and the Chamber of Secrets', authorId: 1 },
-	{ id: 2, name: 'Harry Potter and the Prisoner of Azkaban', authorId: 1 },
-	{ id: 3, name: 'Harry Potter and the Goblet of Fire', authorId: 1 },
-	{ id: 4, name: 'The Fellowship of the Ring', authorId: 2 },
-	{ id: 5, name: 'The Two Towers', authorId: 2 },
-	{ id: 6, name: 'The Return of the King', authorId: 2 },
-	{ id: 7, name: 'The Way of Shadows', authorId: 3 },
-	{ id: 8, name: 'Beyond the Shadows', authorId: 3 }
+const songs = [
+	{ id: 1, title: 'Tu Me Dejaste De Querer', release: 2021, album: 'El Madrileno',genre: 'flamenco' ,authorId: 1 },
+  { id: 2, title: 'Do I Wanna Know?', release: 2013, album: 'AM',genre: 'rock' ,authorId: 2 },
+  { id: 3, title: 'R U Mine?', release: 2013, album: 'AM',genre: 'rock' ,authorId: 2 },
+  { id: 4, title: 'Nothing else matters', release: 1991, album: 'Metallica',genre: 'rock' ,authorId: 3 },
+  { id: 5, title: 'Master Of Puppets', release: 1986, album: 'Master of Puppets',genre: 'rock' ,authorId: 3 },
 ]
 
-const BookType = new GraphQLObjectType({
-  name: 'Book',
-  description: 'This represents a book written by an author',
+const SongType = new GraphQLObjectType({
+  name: 'Song',
+  description: 'This represents a song written by an author',
   fields: () => ({
     id: { type: GraphQLNonNull(GraphQLInt) },
-    name: { type: GraphQLNonNull(GraphQLString) },
+    title: { type: GraphQLNonNull(GraphQLString) },
+    release: { type: GraphQLNonNull(GraphQLInt) },
+    album: { type: GraphQLNonNull(GraphQLString) },
+    genre: { type: GraphQLNonNull(GraphQLString) },
     authorId: { type: GraphQLNonNull(GraphQLInt) },
     author: {
       type: AuthorType,
-      resolve: (book) => {
-        return authors.find(author => author.id === book.authorId)
+      resolve: (song) => {
+        return authors.find(author => author.id === song.authorId)
       }
     }
   })
@@ -45,15 +45,16 @@ const BookType = new GraphQLObjectType({
 
 const AuthorType = new GraphQLObjectType({
   name: 'Author',
-  description: 'This represents a author of a book',
+  description: 'This represents an author of a song',
   fields: () => ({
     id: { type: GraphQLNonNull(GraphQLInt) },
     name: { type: GraphQLNonNull(GraphQLString) },
     email: { type: GraphQLNonNull(GraphQLString) },
-    books: {
-      type: new GraphQLList(BookType),
+    homeTown: { type: GraphQLNonNull(GraphQLString) },
+    songs: {
+      type: new GraphQLList(SongType),
       resolve: (author) => {
-        return books.filter(book => book.authorId === author.id)
+        return songs.filter(song => song.authorId === author.id)
       }
     }
   })
@@ -63,18 +64,18 @@ const RootQueryType = new GraphQLObjectType({
   name: 'Query',
   description: 'Root Query',
   fields: () => ({
-    book: {
-      type: BookType,
-      description: 'A Single Book',
+    song: {
+      type: SongType,
+      description: 'A song',
       args: {
         id: { type: GraphQLInt }
       },
-      resolve: (parent, args) => books.find(book => book.id === args.id)
+      resolve: (parent, args) => songs.find(song => song.id === args.id)
     },
-    books: {
-      type: new GraphQLList(BookType),
-      description: 'List of All Books',
-      resolve: () => books
+    songs: {
+      type: new GraphQLList(SongType),
+      description: 'List of all songs',
+      resolve: () => songs
     },
     authors: {
       type: new GraphQLList(AuthorType),
@@ -96,39 +97,32 @@ const RootMutationType = new GraphQLObjectType({
   name: 'Mutation',
   description: 'Root Mutation',
   fields: () => ({
-    addBook: {
-      type: BookType,
-      description: 'Add a book',
+    addSong: {
+      type: SongType,
+      description: 'Add a new song',
       args: {
-        name: { type: GraphQLNonNull(GraphQLString) },
+        title: { type: GraphQLNonNull(GraphQLString) },
+        release: { type: GraphQLNonNull(GraphQLInt) },
+        album: { type: GraphQLNonNull(GraphQLString) },
+        genre: { type: GraphQLNonNull(GraphQLString) },
         authorId: { type: GraphQLNonNull(GraphQLInt) }
       },
       resolve: (parent, args) => {
-        const book = { id: books.length + 1, name: args.name, authorId: args.authorId }
-        books.push(book)
-        return book
+        const song = { id: songs.length + 1, title: args.title,release: args.release,album:args.album,genre:args.genre, authorId: args.authorId }
+        songs.push(song)
+        return song;
       }
     },
-    deleteBook: {
-            type:BookType,
-            description: "Delete a book",
-            args:{
-                id:{type: new GraphQLNonNull(GraphQLString)}
-            },
-            resolve(parentValue, args){
-                return axios.delete('http://localhost:3000/customers/'+args.id)
-                .then(res => res.data);
-            }
-        },
     addAuthor: {
       type: AuthorType,
       description: 'Add an author',
       args: {
         name: { type: GraphQLNonNull(GraphQLString) },
-        email: { type: GraphQLNonNull(GraphQLString) }
+        email: { type: GraphQLNonNull(GraphQLString) },
+        homeTown: { type: GraphQLNonNull(GraphQLString) },
       },
       resolve: (parent, args) => {
-        const author = { id: authors.length + 1, name: args.name, email: args.email }
+        const author = { id: authors.length + 1, name: args.name, email: args.email, homeTown: args.homeTown }
         authors.push(author)
         return author
       }
